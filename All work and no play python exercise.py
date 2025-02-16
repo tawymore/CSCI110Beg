f62fa4cb7cf127729ad79a48103a85a3d51c9c2c
tawymore <tawymore@mavs.coloradomesa.edu>
2025-02-15 17:42:54 -0800

    Add files via upload

---
+++ All work and no play python exercise.py
@@ -0,0 +1,11 @@
+a = """all"""
+b = """work"""
+c = """and"""
+d = """no"""
+e = """play"""
+f  = """makes"""
+g = """jack"""
+h = """a"""
+i = """dull"""
+j = """boy"""
+print (a,b,c,d,e,f,g,h,i,j)
